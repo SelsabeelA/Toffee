7888af4a6a2a0a7a78c8a9fe994a5b526c907a43
Basma2423 <92788812+FlouBsy@users.noreply.github.com>
2023-05-04 20:08:12 +0300

    getter methods added

--- Payment/Payment.java
+++ Payment/Payment.java
@@ -3,68 +3,79 @@ package Payment;
 import java.util.Date;
 
 enum PaymentMethod {
-	EWallet,
-	OnDelivery,
-	Voucher
+    EWallet,
+    OnDelivery,
+    Voucher
 }
 
-enum PaymentStatus{
-	Pending,
-	Completed,
-	Cancelled
+enum PaymentStatus {
+    Pending,
+    Completed,
+    Cancelled
 }
 
 public class Payment {
- private int paymentID;
- private int ewalletNumber;
- private String paymentCode;
- private double amount;
- private Date paymentDate;
- private PaymentMethod method;
- private PaymentStatus status;
- 
- public Payment(int myPaymentID, int myEwalletNumber, String myPaymentCode, double myAmount, Date myPaymentDate, PaymentMethod myMethod, PaymentStatus myStatus) {
-     this.paymentID = myPaymentID;
-     this.ewalletNumber = myEwalletNumber;
-     this.paymentCode = myPaymentCode;
-     this.amount = myAmount;
-     this.paymentDate = myPaymentDate;
-     this.method = myMethod;
-     this.status = myStatus;
- }
- 
- public Order processPayment(ShoppingCart s) {
-	 
- }
+    private int paymentID;
+    private int ewalletNumber;
+    private String paymentCode;
+    private double amount;
+    private Date paymentDate;
+    private PaymentMethod method;
+    private PaymentStatus status;
 
- public void cancelPayment(ShoppingCart s){
-	 //code
- }
- public void generatePaymentCode(){
-	 //code
-}
- public void sendOTP(int phonenum) {
-	 //code to send OTP
- }
- private boolean verifyOTP(int otpcode) {
-     // code to verify OTP
-     return true;
- }
+    public Payment(int myPaymentID, int myEwalletNumber, String myPaymentCode, double myAmount, Date myPaymentDate, PaymentMethod myMethod, PaymentStatus myStatus) {
+        this.paymentID = myPaymentID;
+        this.ewalletNumber = myEwalletNumber;
+        this.paymentCode = myPaymentCode;
+        this.amount = myAmount;
+        this.paymentDate = myPaymentDate;
+        this.method = myMethod;
+        this.status = myStatus;
+    }
 
- private boolean checkPaymentLimit(double amount) {
-     // code to check payment limit
-     return true;
- }
+    public Order processPayment(ShoppingCart s) {
 
- private boolean verifyPayment() {
-	 if(this.status.equals("Completed")) {
-		 System.out.println("Payment verified!");
-	     return true;
-	 }
-	 else {
-		 return false;
-	 }
- }
+    }
+
+    public void cancelPayment(ShoppingCart s) {
+        //code
+    }
+
+    public void generatePaymentCode() {
+        //code
+    }
+
+    public Date getPaymentDate() {
+        return paymentDate;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void sendOTP(int phonenum) {
+        //code to send OTP
+    }
+
+    private boolean verifyOTP(int otpcode) {
+        // code to verify OTP
+        return true;
+    }
+
+    private boolean checkPaymentLimit(double amount) {
+        // code to check payment limit
+        return true;
+    }
+
+    private boolean verifyPayment() {
+        if (this.status.equals("Completed")) {
+            System.out.println("Payment verified!");
+            new
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 }
 
