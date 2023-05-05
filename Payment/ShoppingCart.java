c3bfe5b1f0abe1591aa465492b7366f95b8ed726
Selsabeel A <90451756+SelsabeelA@users.noreply.github.com>
2023-05-05 12:46:24 +0300

    Update ShoppingCart.java

--- Payment/ShoppingCart.java
+++ Payment/ShoppingCart.java
@@ -5,33 +5,54 @@ import Product.Item;
 import java.util.Map;
 
 public class ShoppingCart {
+	static int random = 0;
     private int cartID;
     private Map<Item, Integer> cartItems;
+    private double total;
+
 
     public void checkout() {
-        new Payment(this);
+    	random++;
+    	String randomString = "" + random;
+        new Payment(random,random, randomString,total, PaymentMethod.CASH_ON_DELIVERY, PaymentStatus.PENDING, this);
     }
 
-    public void addItem(Item cartitem, int quantity) {
-        cartItems.
+
+    public void addItem(Item cartItem, int quantity) {
+        if(cartItems.containsKey(cartItem)) {
+        	int oldQuantity = cartItems.get(cartItem);
+        	cartItems.put(cartItem, oldQuantity + quantity);
+        }
+        else {
+        	cartItems.put(cartItem, quantity);
+        }
+        total +=quantity;
+    }
+    
+    public void removeItem(Item cartItem, int quantity) {
+        if(cartItems.containsKey(cartItem)) {
+        	int oldQuantity = cartItems.get(cartItem);
+        	if((oldQuantity - quantity)>0) {
+            	cartItems.put(cartItem, oldQuantity - quantity);
+        	}
+        	else {
+        		cartItems.remove(cartItem);
+        	}
+        }
+    }
+    
+    public void updateQuantity(Item cartItem, int quantity) {
+    	cartItems.put(cartitem, quantity);
+    }
+    
+    public Map<Item, Integer> getItems(){
+    	return cartItems;
+    }
+    
+    public void clearCart() {
+    	cartItems.clear();
     }
 
 
 }
-/*ShoppingCart
 
--cartID: int
--cartItems: Map<Item, Integer>
-+ checkout() : Order
-
-+addItem(item: CartItem, quantity: int) void
-
-+removeItem(item: CartItem) void
-
-+updateQuantity(item: CartItem, quantity: int) void
-
-+getItems() Map<CartItem, Integer>
-
-+clearCart() void
-
-*/
