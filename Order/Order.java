26d6f22607df161318ce6597a174a1e2208037df
Basma2423 <92788812+FlouBsy@users.noreply.github.com>
2023-05-06 16:18:34 +0300

    Order Implementation was added

--- Order/Order.java
+++ Order/Order.java
@@ -17,5 +17,24 @@ public class Order {
         total = payment.getAmount();
     }
 
+    public int getOrderID() {
+        return id;
+    }
 
+    public double orderTotal() {
+        return total;
+    }
+
+    public void print() {
+        System.out.println("Order ID: " + id + " Date: " + creationDate + " Total = " + total + " $");
+    }
+
+    /*public static void main(String[] args) {
+        Item item = new Item(1, "Milk", "Cow Milk", 1.05, FullStock, "Lamar", new Category(12, "Beverages"));
+        ShoppingCart sc = new ShoppingCart();
+        sc.addItem(item, 2);
+        Payment payment = new Payment(0, 0, "randomString", 2 * 1.05, CASH_ON_DELIVERY, PaymentStatus.PENDING, sc);
+        Order demoOrder = new Order(sc, payment);
+        demoOrder.print();
+    }*/
 }
